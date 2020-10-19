efc0d502c268d699f705a7e8d71ebb95603cd287
Yogi Adianta <yogiadianta0@gmail.com>
2020-10-20 06:45:10 +0700

    Ready to production heroku v:1.0.0

---
+++ README.md
@@ -0,0 +1,9 @@
+## Git Repository
+https://git.heroku.com/yoginodevidly.git
+
+## App Endpoint
+https://yoginodevidly.herokuapp.com/
+
+```
+
+```
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
+require('./startup/prod')(app);
 
 app.get('/', (req, res) => {
   res.send('Movie Vidly');
